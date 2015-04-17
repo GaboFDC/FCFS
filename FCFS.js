05e92e7c0c63a10d07d864a15134e23e16202a94
Gabriel Felipe Diaz Cely <gabo.fdc@gmail.com>
2015-04-17 12:12:03 -0500

    Se arrelgo la lista.
    Se soluciono el inconveniente de la lsita de procesos que no se generaba bien. Se creo metodo para ejecutar procesos, pero no funciona bien con el "sleep"

--- FCFS.js
+++ FCFS.js
@@ -3,47 +3,72 @@ function Node () {
     var execTime;
     var arriveTime;
     var next;
-    console.log("Node");
+//    console.log("Node");
 }
 
 function Queue () {
-    var i;
-    var num;
-//    var processTime;
-    var cab;
-    var p;
-    var aux;
-    var p1;
+    var first;
+    var lenght;
+//    var p;
+//    var aux;
+//    var p1;
     console.log("Queue");
 }
 
-Queue.prototype.newNode = function() {
+/*Queue.prototype.newNode = function() {
     console.log("newNode");
     this.p = new Node();
     console.log("p "+this.p);
     this.p.next = this.p;
     this.cab = new Node();
     this.cab = this.p;
-};
+};*/
 
 Queue.prototype.createProcess = function() {
     console.log("creando procesos... ");
-    var random = Math.floor((Math.random() * 5) + 1);
+    var random = Math.floor((Math.random() * 10) + 1);
     console.log("random: " + random);
+
+    this.first = new Node();
+    this.lenght=6000;
+    var tmp = new Node();
+    tmp = this.first;
     for (i=1;i<=random;i++){
-        console.log("ID " + i);
-//        processTime = Math.floor((Math.random() * 800) + 1);
-        this.p1 = new Node();
-        this.p1.ID = i;
-        this.p1.execTime = Math.floor((Math.random() * 800) + 1);
-        this.p1.arriveTime = Math.floor((Math.random() * 6000) + 1);
-        console.log("proceso: "+this.p1.ID+" timepo rafaga: "+this.p1.execTime+" timepo llegada: "+this.p1.arriveTime);
-        this.cab.next = this.p1;
-        this.p1.next = this.p;
-        this.cab = this.p1;
+//        console.log("ID " + i);
+        var currentProcess = new Node();
+        currentProcess.ID = i;
+        currentProcess.execTime = Math.floor((Math.random() * 800) + 1);
+//	this.lenght = (this.lenght + currentProcess.execTime);
+//	console.log("lenght "+this.lenght);
+        currentProcess.arriveTime = Math.floor((Math.random() * this.lenght) + 1);
+//        console.log("proceso: "+currentProcess.ID+" timepo rafaga: "+currentProcess.execTime+" timepo llegada: "+currentProcess.arriveTime);
+
+        tmp.next = currentProcess;
+        tmp = tmp.next;
     }
 };
 
+Queue.prototype.listProcess = function () {
+    var tmp = new Node();
+    tmp = this.first;
+    while ( tmp ){
+        console.log("proceso: "+tmp.ID+" timepo rafaga: "+tmp.execTime+" timepo llegada: "+tmp.arriveTime+" sig "+tmp.next+" - ");
+        tmp = tmp.next;
+    }
+}
+
+Queue.prototype.FCFS = function() {
+    console.log("FCFS... ");
+    var currentProcess = this.first.next;
+
+    while ( currentProcess ){
+        console.log("Proceso en ejecucion: "+currentProcess.ID );
+        console.log("demoro: " );
+        console.log(" "+currentProcess.execTime);
+        currentProcess = currentProcess.next;
+    }
+};
+/*
 Queue.prototype.main = function () {
     console.log("main");
     this.aux=this.p;
@@ -57,12 +82,12 @@ Queue.prototype.main = function () {
         console.log("aux: "+ this.aux.ID +" p next: "+this.p.next.ID );
 
         console.log("WHILE p1:"+ this.p1.ID +" p:"+this.p.ID );
-/*        while ( this.p1 != this.p ){
+        while ( this.p1 != this.p ){
             console.log("Proceso en ejecucion: "+this.p1.ID );
 //            setTimeout(function() {
                 console.log("demoro: "+this.p1.execTime );
 //            }, ( this.p1.execTime ));
-*/            console.log("p next: "+this.p.next.ID+" aux next: "+this.aux.next.ID);
+            console.log("p next: "+this.p.next.ID+" aux next: "+this.aux.next.ID);
             this.p.next = this.aux.next;
             console.log("p next: "+this.p.next.ID+" aux next: "+this.aux.next.ID);
 
@@ -70,11 +95,13 @@ Queue.prototype.main = function () {
             this.aux = this.p.next;
             console.log("aux "+this.aux.ID+" p next: "+this.p.next.ID);
         console.log("WHILE p1:"+ this.p1.ID +" p:"+this.p.ID );
-/*        }*/
+        }
     }
-};
+};*/
 
 var execute = new Queue();
-execute.newNode();
+//execute.newNode();
 execute.createProcess();
-execute.main();
+execute.listProcess();
+execute.FCFS();
+//execute.main();
